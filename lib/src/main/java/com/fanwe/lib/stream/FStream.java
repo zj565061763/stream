06a2f75b1926d4d3441184d5e2c1e1953864afdd
zjun <565061763@qq.com>
2018-05-25 13:35:05 +0800

    注释

--- lib/src/main/java/com/fanwe/lib/stream/FStream.java
+++ lib/src/main/java/com/fanwe/lib/stream/FStream.java
@@ -1,9 +1,9 @@
 package com.fanwe.lib.stream;
 
 /**
- * 流对象
+ * 流接口
  * <p>
- * 当流对象的代理对象的方法被调用的时候，会触发和代理对象相同tag的流对象的相同方法
+ * 当流接口代理对象的方法被调用的时候，会触发和代理对象相同tag的流对象的相同方法
  */
 public interface FStream
 {
