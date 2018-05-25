813179876b09cc3f41fe5433b714b151618a46b8
zjun <565061763@qq.com>
2018-05-25 12:11:51 +0800

    重构

--- app/src/main/java/com/fanwe/stream/TestTextView.java
+++ app/src/main/java/com/fanwe/stream/TestTextView.java
@@ -13,8 +13,6 @@ import com.fanwe.lib.stream.FStreamManager;
  */
 public class TestTextView extends AppCompatTextView
 {
-    public static final String TAG = TestTextView.class.getSimpleName();
-
     /**
      * 回调代理对象
      */
@@ -28,11 +26,18 @@ public class TestTextView extends AppCompatTextView
             @Override
             public void onClick(View v)
             {
-                setText(String.valueOf(mCallback.getTextViewContent()));
+                /**
+                 * 调用回调代理对象的方法，从注册的流对象中得到一个返回值
+                 */
+                final int result = mCallback.getTextViewContent();
+                setText(String.valueOf(result));
             }
         });
     }
 
+    /**
+     * 回调接口继承流接口
+     */
     public interface Callback extends FStream
     {
         int getTextViewContent();
