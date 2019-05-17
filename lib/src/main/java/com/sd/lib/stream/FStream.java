f1d46dc1375d68688607abf456b2e618cd18f59e
zjun <565061763@qq.com>
2019-05-17 18:01:43 +0800

    DispatchCallback接口参数调整

--- lib/src/main/java/com/sd/lib/stream/FStream.java
+++ lib/src/main/java/com/sd/lib/stream/FStream.java
@@ -80,9 +80,9 @@ public interface FStream
          * @param method       方法
          * @param methodParams 方法参数
          * @param methodResult 方法返回值
-         * @param observer     流对象
+         * @param stream       流对象
          * @return true-停止分发，false-继续分发
          */
-        boolean onDispatch(Method method, Object[] methodParams, Object methodResult, Object observer);
+        boolean onDispatch(Method method, Object[] methodParams, Object methodResult, FStream stream);
     }
 }
