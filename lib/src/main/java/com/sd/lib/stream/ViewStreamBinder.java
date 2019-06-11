31bc3e0b39ab02b0ea81d38b4471239adec1ce40
zhengjun <565061763@qq.com>
2019-06-11 22:40:59 +0800

    StreamBinder

---
+++ lib/src/main/java/com/sd/lib/stream/ViewStreamBinder.java
@@ -0,0 +1,80 @@
+package com.sd.lib.stream;
+
+import android.os.Build;
+import android.view.View;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * 流对象会根据{@link View#isAttachedToWindow()}自动注册和取消注册
+ * <p>
+ * 注意：不要在以下两个地方绑定，否则有可能导致流对象没办法被自动取消注册<br>
+ * 1.绑定View对象的{@link View#onDetachedFromWindow()}方法<br>
+ * 2.监听绑定View对象的{@link View.OnAttachStateChangeListener#onViewDetachedFromWindow(View)}方法
+ */
+class ViewStreamBinder
+{
+    private final WeakReference<FStream> mStream;
+    private final WeakReference<View> mView;
+
+    ViewStreamBinder(FStream stream, View view)
+    {
+        if (stream == null || view == null)
+            throw new IllegalArgumentException("stream or view is null when create " + ViewStreamBinder.class.getName());
+
+        mStream = new WeakReference<>(stream);
+        mView = new WeakReference<>(view);
+
+        view.addOnAttachStateChangeListener(mOnAttachStateChangeListener);
+        registerStream();
+    }
+
+    private final View.OnAttachStateChangeListener mOnAttachStateChangeListener = new View.OnAttachStateChangeListener()
+    {
+        @Override
+        public void onViewAttachedToWindow(View v)
+        {
+            registerStream();
+        }
+
+        @Override
+        public void onViewDetachedFromWindow(View v)
+        {
+            unregisterStream();
+        }
+    };
+
+    private void registerStream()
+    {
+        final FStream stream = mStream.get();
+        if (stream != null && isAttached(mView.get()))
+            FStreamManager.getInstance().registerInternal(stream);
+    }
+
+    private void unregisterStream()
+    {
+        final FStream stream = mStream.get();
+        if (stream != null)
+            FStreamManager.getInstance().unregisterInternal(stream);
+    }
+
+    /**
+     * 取消注册流对象，并解除绑定关系
+     */
+    void destroy()
+    {
+        final View view = mView.get();
+        if (view != null)
+            view.removeOnAttachStateChangeListener(mOnAttachStateChangeListener);
+
+        unregisterStream();
+    }
+
+    private static boolean isAttached(View view)
+    {
+        if (Build.VERSION.SDK_INT >= 19)
+            return view.isAttachedToWindow();
+        else
+            return view.getWindowToken() != null;
+    }
+}
