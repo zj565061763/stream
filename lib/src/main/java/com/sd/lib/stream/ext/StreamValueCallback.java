9d2189d1d96c6a1804e8796c098894ab298ab59d
zjun <565061763@qq.com>
2021-04-06 10:42:41 +0800

    StreamValueCallback format

--- lib/src/main/java/com/sd/lib/stream/ext/StreamValueCallback.java
+++ lib/src/main/java/com/sd/lib/stream/ext/StreamValueCallback.java
@@ -2,8 +2,7 @@ package com.sd.lib.stream.ext;
 
 import androidx.annotation.Nullable;
 
-public interface StreamValueCallback<T>
-{
+public interface StreamValueCallback<T> {
     void onSuccess(@Nullable T value);
 
     void onError(int code, @Nullable String desc);
