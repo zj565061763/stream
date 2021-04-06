d44d68a4b219bc59b7f075a2330b631e0df3cbb8
zjun <565061763@qq.com>
2021-04-06 10:41:18 +0800

    ViewStreamBinder format

--- lib/src/main/java/com/sd/lib/stream/binder/ViewStreamBinder.java
+++ lib/src/main/java/com/sd/lib/stream/binder/ViewStreamBinder.java
@@ -16,66 +16,62 @@ import com.sd.lib.stream.FStream;
  * 1.目标View对象的{@link View#onDetachedFromWindow()}方法<br>
  * 2.监听目标View对象的{@link View.OnAttachStateChangeListener#onViewDetachedFromWindow(View)}方法
  */
-public class ViewStreamBinder extends StreamBinder<View>
-{
-    public ViewStreamBinder(@NonNull FStream stream, @NonNull View target)
-    {
+public class ViewStreamBinder extends StreamBinder<View> {
+    public ViewStreamBinder(@NonNull FStream stream, @NonNull View target) {
         super(stream, target);
     }
 
     @Override
-    public final boolean bind()
-    {
+    public final boolean bind() {
         final View target = getTarget();
-        if (target == null)
+        if (target == null) {
             return false;
+        }
 
         final Context context = target.getContext();
-        if (context instanceof Activity)
-        {
+        if (context instanceof Activity) {
             final Activity activity = (Activity) context;
-            if (activity.isFinishing())
+            if (activity.isFinishing()) {
                 return false;
+            }
         }
 
         target.removeOnAttachStateChangeListener(mOnAttachStateChangeListener);
         target.addOnAttachStateChangeListener(mOnAttachStateChangeListener);
 
-        if (isAttached(target))
+        if (isAttached(target)) {
             return registerStream();
+        }
 
         return true;
     }
 
-    private final View.OnAttachStateChangeListener mOnAttachStateChangeListener = new View.OnAttachStateChangeListener()
-    {
+    private final View.OnAttachStateChangeListener mOnAttachStateChangeListener = new View.OnAttachStateChangeListener() {
         @Override
-        public void onViewAttachedToWindow(View v)
-        {
+        public void onViewAttachedToWindow(View v) {
             registerStream();
         }
 
         @Override
-        public void onViewDetachedFromWindow(View v)
-        {
+        public void onViewDetachedFromWindow(View v) {
             unregisterStream();
         }
     };
 
     @Override
-    public void destroy()
-    {
+    public void destroy() {
         super.destroy();
         final View target = getTarget();
-        if (target != null)
+        if (target != null) {
             target.removeOnAttachStateChangeListener(mOnAttachStateChangeListener);
+        }
     }
 
-    private static boolean isAttached(View view)
-    {
-        if (Build.VERSION.SDK_INT >= 19)
+    private static boolean isAttached(View view) {
+        if (Build.VERSION.SDK_INT >= 19) {
             return view.isAttachedToWindow();
-        else
+        } else {
             return view.getWindowToken() != null;
+        }
     }
 }
