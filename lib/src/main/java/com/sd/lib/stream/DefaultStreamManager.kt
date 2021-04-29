85195424f424643a813f1e0bd406aae12cd862f2
zjun <565061763@qq.com>
2021-04-29 16:51:27 +0800

    DefaultStreamManager

---
+++ lib/src/main/java/com/sd/lib/stream/DefaultStreamManager.kt
@@ -0,0 +1,56 @@
+package com.sd.lib.stream
+
+import com.sd.lib.stream.factory.DefaultStreamFactory
+import com.sd.lib.stream.factory.DefaultStreamFactory.CreateParam
+import com.sd.lib.stream.factory.WeakCacheDefaultStreamFactory
+import com.sd.lib.stream.utils.LibUtils
+import java.util.concurrent.ConcurrentHashMap
+
+object DefaultStreamManager {
+    private val _mapDefaultStreamClass: MutableMap<Class<out FStream>, Class<out FStream>> = ConcurrentHashMap()
+    private var _defaultStreamFactory: DefaultStreamFactory? = null
+
+    /**
+     * 注册默认的流接口实现类
+     */
+    @Synchronized
+    fun registerDefaultStream(clazz: Class<out FStream>) {
+        val set = LibUtils.findAllStreamClass(clazz)
+        require(!set.isEmpty()) { "stream class was not found in $clazz" }
+        for (item in set) {
+            _mapDefaultStreamClass[item] = clazz
+        }
+    }
+
+    /**
+     * 取消注册默认的流接口实现类
+     */
+    @Synchronized
+    fun unregisterDefaultStream(clazz: Class<out FStream?>) {
+        val set = LibUtils.findAllStreamClass(clazz)
+        for (item in set) {
+            _mapDefaultStreamClass.remove(item)
+        }
+    }
+
+    /**
+     * 设置[DefaultStreamFactory]
+     */
+    @Synchronized
+    fun setDefaultStreamFactory(factory: DefaultStreamFactory?) {
+        _defaultStreamFactory = factory
+    }
+
+    /**
+     * 返回默认的流对象
+     */
+    @Synchronized
+    fun getDefaultStream(clazz: Class<out FStream>): FStream? {
+        val defaultClass = _mapDefaultStreamClass[clazz] ?: return null
+
+        if (_defaultStreamFactory == null) {
+            _defaultStreamFactory = WeakCacheDefaultStreamFactory()
+        }
+        return _defaultStreamFactory!!.create(CreateParam(clazz, defaultClass))
+    }
+}
