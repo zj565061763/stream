fa65e971c2cc4c678bf543ba92388517691914dd
zjun <565061763@qq.com>
2021-04-29 09:33:51 +0800

    kotlin StreamBinder

--- lib/src/main/java/com/sd/lib/stream/binder/StreamBinder.kt
+++ lib/src/main/java/com/sd/lib/stream/binder/StreamBinder.kt
@@ -1,35 +1,20 @@
-package com.sd.lib.stream.binder;
+package com.sd.lib.stream.binder
 
-import androidx.annotation.CallSuper;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.CallSuper
+import com.sd.lib.stream.FStream
+import com.sd.lib.stream.FStreamManager
+import java.lang.ref.WeakReference
 
-import com.sd.lib.stream.FStream;
-import com.sd.lib.stream.FStreamManager;
+abstract class StreamBinder<T> {
+    private val _stream: WeakReference<FStream>
+    private val _target: WeakReference<T>
 
-import java.lang.ref.WeakReference;
+    /** 返回要绑定的对象 */
+    val target: T? get() = _target.get()
 
-public abstract class StreamBinder<T> {
-    private final WeakReference<FStream> mStream;
-    private final WeakReference<T> mTarget;
-
-    public StreamBinder(@NonNull FStream stream, @NonNull T target) {
-        if (stream == null || target == null) {
-            throw new IllegalArgumentException("null argument");
-        }
-
-        mStream = new WeakReference<>(stream);
-        mTarget = new WeakReference<>(target);
-    }
-
-    /**
-     * 返回要绑定的对象
-     *
-     * @return
-     */
-    @Nullable
-    public final T getTarget() {
-        return mTarget.get();
+    constructor(stream: FStream, target: T) {
+        _stream = WeakReference(stream)
+        _target = WeakReference(target)
     }
 
     /**
@@ -37,41 +22,33 @@ public abstract class StreamBinder<T> {
      *
      * @return
      */
-    public abstract boolean bind();
+    abstract fun bind(): Boolean
 
     /**
      * 注册流对象
      *
      * @return
      */
-    protected final boolean registerStream() {
-        final FStream stream = mStream.get();
-        if (stream == null) {
-            return false;
-        }
-
-        FStreamManager.getInstance().register(stream);
-        return true;
+    protected fun registerStream(): Boolean {
+        val stream = _stream.get() ?: return false
+        FStreamManager.getInstance().register(stream)
+        return true
     }
 
     /**
      * 取消注册流对象
      */
-    protected final void unregisterStream() {
-        final FStream stream = mStream.get();
-        if (stream == null) {
-            return;
-        }
-
-        FStreamManager.getInstance().unregister(stream);
+    protected fun unregisterStream() {
+        val stream = _stream.get() ?: return
+        FStreamManager.getInstance().unregister(stream)
     }
 
     /**
      * 取消注册流对象，并解除绑定关系
      */
     @CallSuper
-    public void destroy() {
-        unregisterStream();
-        mStream.clear();
+    open fun destroy() {
+        unregisterStream()
+        _stream.clear()
     }
 }
