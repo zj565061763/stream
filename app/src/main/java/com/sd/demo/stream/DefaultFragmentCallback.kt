1a2037b6d52df8d6669353186e559945c47d8241
zjun <565061763@qq.com>
2021-05-21 00:46:58 +0800

    demo

--- app/src/main/java/com/sd/demo/stream/DefaultFragmentCallback.kt
+++ app/src/main/java/com/sd/demo/stream/DefaultFragmentCallback.kt
@@ -1,8 +1,14 @@
 package com.sd.demo.stream
 
 import com.sd.demo.stream.TestFragment.FragmentCallback
+import com.sd.lib.stream.DefaultStreamManager
 import com.sd.lib.stream.FStream
 
+/**
+ * 如果代理对象方法调用的时候未找到流对象，则会用注册的默认流Class创建流对象来触发
+ *
+ * [DefaultStreamManager.register]
+ */
 class DefaultFragmentCallback : FragmentCallback {
     override fun getDisplayContent(): String {
         return "default stream value"
