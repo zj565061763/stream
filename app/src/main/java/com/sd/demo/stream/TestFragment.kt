f6d1feba657cb940889f413309ac3eca70e483ac
zjun <565061763@qq.com>
2021-05-21 00:50:19 +0800

    demo

--- app/src/main/java/com/sd/demo/stream/TestFragment.kt
+++ app/src/main/java/com/sd/demo/stream/TestFragment.kt
@@ -1,43 +1,34 @@
-package com.sd.demo.stream;
+package com.sd.demo.stream
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.sd.lib.stream.FStream
 
-import androidx.fragment.app.Fragment;
+class TestFragment : Fragment() {
+    /** 创建接口代理对象  */
+    private val _callback = FStream.ProxyBuilder()
+        /**
+         * 设置代理对象的tag，默认tag为null
+         * 注意：只有tag和当前代理对象tag相等的流对象才会被通知到
+         */
+        .setTag(null)
+        .build(FragmentCallback::class.java)
 
-import com.sd.lib.stream.FStream;
-
-public class TestFragment extends Fragment {
-    /** 创建接口代理对象 */
-    private final FragmentCallback mCallback = new FStream.ProxyBuilder()
-            /**
-             * 设置代理对象的tag，默认tag为null
-             * 注意：只有tag和当前代理对象tag相等的流对象才会被通知到，tag比较相等的规则为 “==” 或者 “equals”
-             */
-            .setTag(null)
-            .build(FragmentCallback.class);
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final Button button = new Button(getContext());
-        button.setAllCaps(false);
-        button.setText("click");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 获取显示内容
-                final String content = mCallback.getDisplayContent();
-                button.setText(content);
-            }
-        });
-
-        return button;
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val button = Button(context)
+        button.text = "点击"
+        button.setOnClickListener {
+            // 获取显示内容
+            button.text = _callback.getDisplayContent()
+        }
+        return button
     }
 
-    public interface FragmentCallback extends FStream {
-        String getDisplayContent();
+    interface FragmentCallback : FStream {
+        fun getDisplayContent(): String
     }
 }
