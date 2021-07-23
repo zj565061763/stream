2a8177b4c35fbd625b30c02328b3d4fe9019935c
zjun <565061763@qq.com>
2021-07-24 00:30:35 +0800

    DefaultStreamManager

--- lib/src/main/java/com/sd/lib/stream/DefaultStreamManager.kt
+++ lib/src/main/java/com/sd/lib/stream/DefaultStreamManager.kt
@@ -6,16 +6,20 @@ import com.sd.lib.stream.factory.WeakCacheStreamFactory
 import com.sd.lib.stream.utils.LibUtils
 
 /**
- * 默认流接口管理
+ * 默认流class管理
+ *
+ * 如果代理对象方法触发的时候未找到注册的流对象，则会查找是否存在默认流class，
+ * 如果存在默认流class，则用默认流class创建默认流对象来触发，默认流必须包含空构造方法
  */
 object DefaultStreamManager {
+    /** 映射流接口和默认流class */
     private val _mapDefaultStreamClass: MutableMap<Class<out FStream>, Class<out FStream>> = HashMap()
 
-    /** 默认流接口对象工厂 */
+    /** 默认流对象工厂 */
     var streamFactory: DefaultStreamFactory = WeakCacheStreamFactory()
 
     /**
-     * 注册默认的流接口实现类
+     * 注册默认流class
      */
     @Synchronized
     fun register(defaultClass: Class<out FStream>) {
@@ -26,7 +30,7 @@ object DefaultStreamManager {
     }
 
     /**
-     * 取消注册默认的流接口实现类
+     * 取消注册默认流class
      */
     @Synchronized
     fun unregister(defaultClass: Class<out FStream>) {
@@ -37,7 +41,7 @@ object DefaultStreamManager {
     }
 
     /**
-     * 返回默认的流对象
+     * 返回默认流对象
      */
     @Synchronized
     internal fun getStream(clazz: Class<out FStream>): FStream? {
